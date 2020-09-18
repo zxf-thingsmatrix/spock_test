3111450379acf31d585bfaf1e055424ac65e8f5b
xinfan.zhou <xinfan.zhou@things-matrix.com>
2020-09-18 18:56:06 +0800

    sc

---
+++ src/test/groovy/lost/canvas/spock_test/SpockTest.groovy
@@ -0,0 +1,80 @@
+package lost.canvas.spock_test
+
+import lost.canvas.spock_test.Calc
+import spock.lang.Specification
+
+class SpockTest extends Specification {
+
+    def setup() {
+        println('>>> setup')
+    }
+
+    def cleanup() {
+        println('>>> cleanup')
+    }
+
+    def setupSpec() {
+        println('>>>>>> setupSpec')
+    }
+
+    def cleanupSpec() {
+        println('>>>>>> cleanupSpec')
+    }
+
+    def "test given when then"() {
+        given:
+        def a = 1
+        when:
+        a++
+        then:
+        a == 2
+    }
+
+    def "test expect where"() {
+        expect:
+        calc(a, b) == c
+        where:
+        a | b | c
+        1 | 2 | 3
+        1 | 0 | 1
+        0 | 1 | 1
+    }
+
+    def "test expect where 2"() {
+        expect:
+        calc(a, b) == c
+        where:
+        a << [1, 1, 0]
+        b << [2, 0, 1]
+        c << [3, 1, 1]
+    }
+
+    def calc(a, b) {
+        a + b
+    }
+
+    /**
+     * Groovy Java 混编
+     * 1. 可以通过 new 创建 Java 对象
+     * 2. 可以调用 Java 对象的方法
+     * 3. 可以 implements Java 接口
+     * 4. 可以 extends Java 类
+     * @return
+     */
+    def "test java"() {
+        given: "invoke java Calc.add"
+        def calc = new Calc()
+        expect:
+        calc.add(a, b) == c
+        calc.div(x, y) == z
+        where:
+        a << [1, 2, 3]
+        b << [3, 2, 1]
+        c << [4, 4, 4]
+
+        x << [7, 8, 9]
+        y << [1, 2, 3]
+        z << [6, 6, 6]
+    }
+
+}
---
+++ src/test/java/lost/canvas/spock_test/Calc.java
@@ -0,0 +1,11 @@
+package lost.canvas.spock_test;
+
+public class Calc {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int div(int a, int b) {
+        return a - b;
+    }
+}
